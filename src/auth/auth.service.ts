927d7b983a75d4d1042f4f49c83c6a9a6e486c58
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-29 20:53:07 -0300

    feat: adding jwtExtractor method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Request } from 'express';
+import { sign } from 'jsonwebtoken';
+import { Model } from 'mongoose';
+import { User } from 'src/users/models/users.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel('User')
+    private readonly usersModel: Model<User>,
+  ) {}
+
+  public async createAcessToken(userId: string): Promise<string> {
+    return sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,
+    });
+  }
+
+  public async validateUser(userId: string): Promise<User> {
+    const user = await this.usersModel.find({ _id: userId });
+    if (!user) throw new UnauthorizedException('User not found');
+    return user;
+  }
+
+  private jwtExtractor(request: Request): string {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) throw new BadRequestException('Bad Request');
+
+    const [, token] = authHeader.split(' ');
+
+    return token;
+  }
+}
