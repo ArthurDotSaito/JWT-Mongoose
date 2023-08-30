d83f42be93d17997fd1c78e7e07917e9de295328
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-30 19:06:30 -0300

    feat: implementing signUp method in user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from './models/users.model';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AuthService } from 'src/auth/auth.service';
+import { SignupDTO } from './dto/signup-dto';
+import { SignInDTO } from './dto/signin-dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel('User')
+    private readonly usersModel: Model<User>,
+    private readonly authService: AuthService,
+  ) {}
+
+  public async signUp(signUpDto: SignupDTO): Promise<User> {
+    const user = new this.usersModel(signUpDto);
+    return user.save();
+  }
+}
