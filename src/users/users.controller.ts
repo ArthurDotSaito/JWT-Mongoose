47a01493a85c1ec65c8927cd03d12a986070fba3
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-30 19:21:57 -0300

    feat: implementing signIn and signUp controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignupDTO } from './dto/signup-dto';
+import { User } from './models/users.model';
+import { SignInDTO } from './dto/signin-dto';
+import { UserSignIn } from './types/UserSignIn';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly UsersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async signUp(@Body() signUpDto: SignupDTO): Promise<User> {
+    return this.UsersService.signUp(signUpDto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signIn(@Body() signInDto: SignInDTO): Promise<UserSignIn> {
+    return this.UsersService.signIn(signInDto);
+  }
+}
