b04f0e7f5c539b68ca32e1f9e765bd2b37ccc622
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-30 19:12:29 -0300

    implementing signIn method and check on users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { User } from './models/users.model';
 import { Model } from 'mongoose';
@@ -18,5 +18,28 @@ export class UsersService {
   public async signUp(signUpDto: SignupDTO): Promise<User> {
     const user = new this.usersModel(signUpDto);
     return user.save();
+  }
+  public async signIn(
+    signInDto: SignInDTO,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    const user = await this.findByEmail(signInDto.email);
+    const match = await this.checkPassword(signInDto.password, user);
+    if (!user) throw new NotFoundException('Invalid Credentials');
+
+    const jwtToken = await this.authService.createAcessToken(user._id);
+
+    return { name: user.name, jwtToken, email: user.email };
+  }
+
+  private async findByEmail(email: string): Promise<User> {
+    const user = await this.usersModel.findOne({ email });
+    if (!user) throw new NotFoundException('Email not Found');
+    return user;
+  }
+
+  private async checkPassword(password: string, user: User): Promise<boolean> {
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) throw new NotFoundException('Password not Found');
+    return match;
   }
 }
